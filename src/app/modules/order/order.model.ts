b0847b8f514b6e39c71da87926ced13e7b3fe7d5
ShahariarRahman <shahariarrahman98@gmail.com>
2023-06-23 08:50:30 +0600

    Order Model Added

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+import { IOrder, OrderModel } from "./order.interfaces";
+
+const OrderSchema = new Schema<IOrder, OrderModel>(
+  {
+    cow: {
+      type: Schema.Types.ObjectId,
+      ref: "Cow",
+      required: true,
+    },
+    buyer: {
+      type: Schema.Types.ObjectId,
+      ref: "Buyer",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+export const Order = model<IOrder, OrderModel>("Order", OrderSchema);
