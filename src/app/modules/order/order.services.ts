926e4dbe26b9a5ea1d2c8542a6849037779f5fa6
ShahariarRahman <shahariarrahman98@gmail.com>
2023-06-26 07:25:10 +0600

    Bugs have been fixed

--- src/app/modules/order/order.services.ts
+++ src/app/modules/order/order.services.ts
@@ -8,40 +8,47 @@ import mongoose from "mongoose";
 
 const createOrder = async (order: IOrder): Promise<IOrder | null> => {
   const buyer = await User.findById(order.buyer);
+  if (!buyer) {
+    throw new ApiError(404, "Buyer not found");
+  }
+
   const cow = await Cow.findById(order.cow);
+  if (!cow) {
+    throw new ApiError(404, "Cow not found");
+  }
 
-  if (buyer && cow) {
-    if (buyer.budget >= cow.price) {
-      const session = await mongoose.startSession();
+  if (buyer.budget >= cow.price) {
+    const session = await mongoose.startSession();
+    try {
+      session.startTransaction();
+      // cow sold out
+      const updateDoc = { label: "sold out" };
+      const cowLevel = await Cow.findByIdAndUpdate(cow._id, updateDoc, {
+        session,
+      });
 
-      try {
-        session.startTransaction();
-        // Change the cow's label from 'for sale' to 'sold out'.
-        cow.label = "sold out";
-        const cowLevel = await Cow.findByIdAndUpdate(cow._id, cow, { session });
-
-        if (!cowLevel) {
-          throw new ApiError(404, "Failed to update cow label");
-        }
-        // create order for user
-        const newOrder = await Order.create(order);
-        const orderData = await Order.findById(newOrder._id)
-          .populate("cow")
-          .populate("buyer");
-
-        session.commitTransaction();
-        session.endSession();
-
-        return orderData;
-      } catch (error) {
-        session.abortTransaction();
-        throw error;
+      if (!cowLevel) {
+        throw new ApiError(404, "Failed to update cow label");
       }
+
+      // create order
+      const newOrder = await Order.create(order);
+
+      await session.commitTransaction();
+      await session.endSession();
+
+      const populatedOrder = await Order.findById(newOrder._id)
+        .populate("cow")
+        .populate("buyer");
+
+      return populatedOrder;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
     }
   } else {
     throw new ApiError(httpStatus.NOT_FOUND, "Buyer has insufficient budget");
   }
-  return null;
 };
 
 export const OrderService = {
